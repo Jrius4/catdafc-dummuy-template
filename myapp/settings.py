462a72489e202674c1409d626a5505d9336563fd
Kazibwe Julius Junior <kazibwejuliusjunior@gmail.com>
2019-08-06 00:00:33 +0300

    gitignored db

--- myapp/settings.py
+++ myapp/settings.py
@@ -115,12 +115,11 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
+DATABASES = { 
+    'default': { 
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+     }}
 
 
 AUTHENTICATION_BACKENDS = (
